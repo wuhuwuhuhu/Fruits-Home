6aa2c17e66d23afdb3db140315b03374897e25c2
whd <wuhd1234@gmail.com>
2021-08-31 23:26:53 -0400

    add: mock data locally

---
+++ src/store/fruit-images.js
@@ -0,0 +1,18 @@
+const fruit_images = [
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/apple.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/apricot.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/banana.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/blueberry.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/cherry.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/guava.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/lemon.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/mango.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/orange.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/pear.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/pineapple.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/raspberry.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/strawberry.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/tomato.png',
+	'https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/watermelon.png',
+];
+export default fruit_images;
